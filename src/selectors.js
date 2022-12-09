f9abec835f391ef78db7fd7ca023b6d06647cf47
Jānis Alunāns <j.alunans@gmail.com>
2022-12-09 21:24:13 +0200

    Add selectors

---
+++ src/selectors.js
@@ -0,0 +1,7 @@
+const tasks = document.querySelector(".tasks")
+const dates = document.querySelector(".dates")
+const projects = document.querySelector(".projects")
+const nav = document.querySelector("nav")
+
+
+export {tasks, dates, projects, nav};
