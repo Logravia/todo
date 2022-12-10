414c34f9fb0f97a7d0b2bace16cbcacbe0d72472
Jānis Alunāns <j.alunans@gmail.com>
2022-12-10 19:07:52 +0200

    Add Display

---
+++ src/display.js
@@ -0,0 +1,6 @@
+import {nav, tasks, dates, projects} from "./selectors"
+class Display {
+  constructor(taskManager) {
+
+  }
+}
--- src/task.js
+++ src/task.js
@@ -1,11 +1,20 @@
 class Task {
-  constructor(name, due, priority="medium", project="", done=false) {
+
+  constructor(name, due, priority = "medium", project = "", done = false) {
     this.name = name;
     this.due = due;
     this.priority = priority;
     this.project = project
-    this.done = done;
+    this.#done = done;
+  }
+
+  get done() {
+    return this.#done
+  }
+
+  toggleDone = ()=> {
+    this.#done = !this.#done;
   }
 }
 
-export {Task}
+export { Task }
