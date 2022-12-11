f0aef3c5d153d6edfed5d1a639327c9abe3192ed
Jānis Alunāns <j.alunans@gmail.com>
2022-12-12 01:59:31 +0200

    Listen to input

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,6 @@
-import {input} from "./selectors.js"
+import {input, tasks} from "./selectors.js"
 import {Task} from "./task.js"
+import {newTaskEdit} from "./taskbox.js"
 
 class InputManager {
   constructor (taskManager, display) {
@@ -19,6 +20,9 @@ class InputManager {
   cleanUp () {
     this.display.clearTasks()
     this.display.show(this.taskManager.taskArray());
+    this.setUpDel();
+    this.setUpEdit();
+    this.setUpCheckbox();
   }
 
   setUpAddButton () {
@@ -27,6 +31,46 @@ class InputManager {
       this.cleanUp()
     })
   }
+
+  setUpDel () {
+    let delBtns = document.querySelectorAll(".deleteBtn");
+    delBtns.forEach((btn)=>{
+      btn.addEventListener("click",(e)=>{
+        let id = e.target.dataset.id;
+        this.taskManager.remove(id);
+        this.cleanUp();
+      })
+    })
+
+  }
+
+  setUpCheckbox () {
+    let checkboxes = document.querySelectorAll(".checkbox");
+    checkboxes.forEach((btn)=>{
+      btn.addEventListener("click",(e)=>{
+        let id = e.target.dataset.id;
+        this.taskManager.finishTask(id);
+        this.cleanUp();
+      })
+    })
+
+  }
+
+  setUpEditSpace (target, task) {
+    target.replaceWith(newTaskEdit(task));
+  }
+
+  setUpEdit () {
+    let editBtns = document.querySelectorAll(".editBtn");
+    editBtns.forEach((btn)=>{
+      btn.addEventListener("click",(e)=>{
+        let id = e.target.dataset.id;
+        let container = tasks.querySelector("#t"+id)
+        this.setUpEditSpace(container, this.taskManager.getTask(id))
+      })
+    })
+  }
+
 }
 
 export {InputManager}
