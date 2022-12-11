7e7b652815c7205bdfd79d4a20a09f64d3236f1e
Jānis Alunāns <j.alunans@gmail.com>
2022-12-12 01:58:22 +0200

    Add html generator for edit window

--- src/taskbox.js
+++ src/taskbox.js
@@ -1,29 +1,30 @@
 import {tasks} from "./selectors";
+import { format } from 'date-fns';
 
-const newTaskHTML = (task, id) =>{
-  tasks.appendChild(newTaskDescBox(task, id));
+const newTaskHTML = (task) =>{
+  tasks.appendChild(newTaskDescBox(task));
 }
 
-const newTaskDescBox = (task, id) => {
+const newTaskDescBox = (task) => {
   let taskDescBox = document.createElement("div");
   taskDescBox.className = "task-desc-box";
-  taskDescBox.appendChild(newTaskBox(task, id));
+  taskDescBox.appendChild(newTaskBox(task));
   taskDescBox.appendChild(newTaskDesc(task));
   return taskDescBox;
 }
 
-const newTaskBox = (task, id) => {
+const newTaskBox = (task) => {
   let container = document.createElement("div")
   container.className = "task-and-buttons";
   container.innerHTML = `
-  <div class="task">
+  <div class="task" id="t${task.id}">
     <p>${task.name}</p>
-    <div class="checkbox"></div>
+    <div class="checkbox" data-id=${task.id}></div>
   </div>
 
   <div class="delete-edit">
-    <div class="deleteBtn button" data-id="${id}">D</div>
-    <div class="editBtn button" data-id="${id}">E</div>
+    <div class="deleteBtn button" data-id="${task.id}">D</div>
+    <div class="editBtn button" data-id="${task.id}">E</div>
   </div>
   `
 
@@ -42,4 +43,21 @@ const newTaskDesc = (task) => {
   return taskDesc;
 }
 
-export {newTaskHTML}
+const newTaskEdit = (task) => {
+  let taskEdit = document.createElement("div");
+  taskEdit.className = "task-edit"
+  taskEdit.innerHTML =
+ `  <label for="edit-name">Name: </label>
+    <input type="text" name="edit-name" id="edit-name" value="${task.name}" />
+    <label for="edit-date">Due: </label>
+    <input type="date" name="edit-date" id="edit-date" value="${format(task.due, "yyyy-MM-dd")}" />
+    <label for="edit-project">Projects: </label>
+    <select name="projects" id="edit-project">
+      <option value="${task.project}">Area 51</option>
+    </select>
+    <div class="button" id="save-edit" data-id="task.id">S</div>`;
+  return taskEdit;
+}
+
+
+export {newTaskHTML, newTaskEdit}
